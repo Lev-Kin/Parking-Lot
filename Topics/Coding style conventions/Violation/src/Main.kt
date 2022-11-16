6db6e02ab1025b46b06a36e82c106152296c08c9
Leva <parimalleva@gmail.com>
2022-11-16 12:30:46 +0300

    1 of 5 stages completed

---
+++ Topics/Coding style conventions/Violation/src/Main.kt
@@ -0,0 +1,1 @@
+fun main() { println("fun main()\n{println(\"Hello, world!\");}") }
---
+++ Topics/Data types/Think as a compiler/src/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val first: String = "Oleg"
+    val last: String = "Potemkin"
+    val age: Int = 42
+    // put your code above
+    println("My name is $first $last and I’m $age years old")
+}
